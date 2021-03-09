099fa7a3bd9873735ef1a69125b939e257e0b39f
Dave Welsh <dave@qccareerschool.com>
2021-03-09 10:16:06 -0500

    Update CourseRow to match shopping cart

--- src/components/CourseRow.tsx
+++ src/components/CourseRow.tsx
@@ -15,12 +15,12 @@ type Props = {
 export const CourseRow: React.FC<Props> = ({ name, currencySymbol, cost, multiCourseDiscount, free }) => (
   <>
     <tr>
-      <td>{name}</td>
-      <td><Align align="right">{free ? <Highlight><strong>FREE</strong></Highlight> : <>{currencySymbol}{cost.toFixed(2)}</>}</Align></td>
+      <td>{name}{free && <Highlight><strong> FREE!</strong></Highlight>}</td>
+      <td><Align align="right">{currencySymbol}{(free ? 0 : cost).toFixed(2)}</Align></td>
     </tr>
     {!free && multiCourseDiscount > 0 && (
       <tr>
-        <td><Highlight>{Math.round(multiCourseDiscount / cost * 100)}% Off</Highlight></td>
+        <td><Highlight>{Math.round(multiCourseDiscount / cost * 100)}% Discount</Highlight></td>
         <td><Align align="right"><WhiteSpace whiteSpace="nowrap"><Highlight>&minus;{currencySymbol}{multiCourseDiscount.toFixed(2)}</Highlight></WhiteSpace></Align></td>
       </tr>
     )}
